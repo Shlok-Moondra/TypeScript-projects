8b9c363f384e84f0bf5f20d768aa0e6454bbb916
Shlok <shlokmoondra5433@gmail.com>
2026-01-10 12:04:53 +0530

    DataTypes and UnionAndAny practice

---
+++ Learning-TypeScript/src/TypeInTs.ts
@@ -0,0 +1,18 @@
+
+//All these are Inferencing
+let chai = "drink"
+// this shows that chai is a string 
+
+let cups = Math.random() > 0.5? 10:5
+//this shows the cup holds number datatype
+
+let cup = Math.random() > 0.5 ? 10: "5"
+//this shows that the variable cup could hold number as well as string
+
+
+
+
+// All these are Annotations
+let name:string = "shlok"
+let marks: number = 20
+let Is_pass:boolean = true
---
+++ Learning-TypeScript/src/unionAndAny.ts
@@ -0,0 +1,26 @@
+
+// this is a standard example of union by default,or predefined datatypes
+let codeNo: string | number = "1M"
+console.log(codeNo)
+
+
+// these are custom dataTypes we are providing
+let apiResponse : "pending" | "success" | "error" = "pending"
+apiResponse = "pending"
+console.log(apiResponse)
+
+
+let numList = ["10","20","28","30"]
+
+
+//let currFolder = in this it is showing ANY means any value could come
+let currFolder: string | undefined;// this shows only few valuescan come
+
+for(let order of numList){
+    if(order == "30"){
+        currFolder = order;
+        break;
+    }
+}
+
+console.log(currFolder);
